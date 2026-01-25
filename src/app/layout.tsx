dfb869bde2c6b48bacd82fc6eb476880148cda53
Ryan Wang <r728wang@uwaterloo.ca>
2026-01-25 14:14:14 -0500

    added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Ryan Wang | Portfolio",
   description: "Mechatronics engineering @ UWaterloo",
+  icons: {
+    icon: "/cat.png",
+  },
 };
 
 export default function RootLayout({
